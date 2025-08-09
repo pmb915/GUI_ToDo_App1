10e36dc10c27da9e57b61c075ccfb52c6990184b
pmb915 <parashar915@gmail.com>
2025-08-09 11:23:05 -0400

    Added Add button functionality in gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,15 @@
 
 from  support_func import clear_terminal , write_file, populate_todo_list , display_todo_list
 import FreeSimpleGUI as sg
+import os
 
+current_location = os.getcwd()
+# current_code_file_name = os.path.basename(__file__)
+todo_file =os.path.join(current_location, 'data',"todo_file.txt")
+
+sg.theme('DarkAmber')
 label = sg.Text("Type in a to-do:")
-input_box = sg.InputText(tooltip="Enter to-do action")
+input_box = sg.InputText(tooltip="Enter to-do action", key='todo')
 add_button = sg.Button("Add")
 # remove_button = sg.Button("Remove")
 # edit_button = sg.Button("Edit")
@@ -12,15 +18,36 @@ add_button = sg.Button("Add")
 exit_button = sg.Button("Quit")
 
 # define the layout
-my_layout = [[label], [input_box, add_button], [exit_button]]
+my_layout = [[label],
+             [input_box, add_button],
+             [exit_button]]
 
 # Create the window
-window = sg.Window("My To-Do Application", layout=my_layout)
+window = sg.Window("My To-Do Application",
+                   layout=my_layout,
+                   font=('Helvetica', 20))
 while True:
+    clear_terminal()
     event, values = window.read()
     # if your want to Quit or close Windows
-    if (event == sg.WIN_CLOSED)  or event == "Quit":
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todo_list = populate_todo_list(todo_file)
+            print( "Add :- ", values['todo'])
+            todo_list.append(values['todo']+'\n')
+            write_file(todo_file, todo_list)
+            print(todo_list)
+        case "Quit" | sg.WIN_CLOSED :
+            print("Closing the application")
+            break
+        case _:
+            pass
+    """
+    if ((event == sg.WIN_CLOSED)  or ( event == "Quit") ):
         break
+    """
 
 
 

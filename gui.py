95d7f1d3aacadbe2d3883a7a6814b9668fc07467
pmb915 <parashar915@gmail.com>
2025-08-09 13:39:11 -0400

    Edit and todolist option code is added

--- gui.py
+++ gui.py
@@ -11,8 +11,12 @@ sg.theme('DarkAmber')
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter to-do action", key='todo')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(tooltip="Display and let select ToDo's for edit/removal",
+                      size=(44,8), key='todolist', enable_events=True,
+                      values=populate_todo_list(todo_file))
+edit_button = sg.Button("Edit")
 # remove_button = sg.Button("Remove")
-# edit_button = sg.Button("Edit")
+
 # show_button = sg.Button("Show")
 # clear_button = sg.Button("Clear")
 exit_button = sg.Button("Quit")
@@ -20,6 +24,7 @@ exit_button = sg.Button("Quit")
 # define the layout
 my_layout = [[label],
              [input_box, add_button],
+             [list_box, edit_button],
              [exit_button]]
 
 # Create the window
@@ -35,10 +40,40 @@ while True:
     match event:
         case "Add":
             todo_list = populate_todo_list(todo_file)
-            print( "Add :- ", values['todo'])
-            todo_list.append(values['todo']+'\n')
-            write_file(todo_file, todo_list)
+            print( "Add :- ", values['todo']+'\n')
+            new_item = values['todo']+'\n'
+            try:
+                index_to_add = todo_list.index(new_item)
+                print('Index to add:', index_to_add)
+                print(f"Todo item : {new_item.strip('\n')}, is already exists!!")
+            except ValueError:
+                # print('Index to add:', index_to_add)
+                todo_list.append(new_item)
+                write_file(todo_file, todo_list)
+                window['todolist'].update(values=todo_list)
+                print(todo_list)
+
+
+        case "Edit":
+            # item_to_edit = values['todolist'][0].replace('\n','')
+            item_to_edit = values['todolist'][0]
+            new_item = values['todo']+'\n'
+            todo_list = populate_todo_list(todo_file)
             print(todo_list)
+            try:
+                index_to_edit = todo_list.index(item_to_edit)
+                todo_list[index_to_edit] = new_item
+                write_file(todo_file, todo_list)
+                print(f"item_to_edit = {item_to_edit.strip('\n')}, is replaced with: {new_item.strip('\n')}")
+                values['todo']
+                window['todolist'].update(values=todo_list)
+                # window['todo'].update(values=None)
+            except ValueError as e:
+                print(f"Error: {e}")
+
+        case 'todolist' :
+            window['todo'].update(value = values['todolist'][0])
+            
         case "Quit" | sg.WIN_CLOSED :
             print("Closing the application")
             break
